d50793c98c82aa1fe1351e29b17c7de1adfdd849
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-30 03:07:15 -0300

    Setup typeorm

---
+++ ormconfig.ts
@@ -0,0 +1,33 @@
+import "./src/config/env";
+import dotenv from "dotenv";
+import path from "path";
+
+const { NODE_ENV } = process.env;
+
+const envPath = NODE_ENV === "test" ? "local.test.env" : "local.dev.env";
+dotenv.config({ path: path.resolve(".", envPath) });
+
+const {
+  DB_HOST,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_DATABASE,
+  DB_PORT,
+  DB_DIALECT,
+  DATABASE_URL,
+} = process.env;
+
+export = {
+  type: DB_DIALECT,
+  url:
+    DATABASE_URL ||
+    `${DB_DIALECT}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  migrationsTableName: "migrations",
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/migrations/*.ts"],
+  cli: {
+    migrationsDir: "src/migrations",
+    entitiesDir: "src/entities/*.ts",
+  },
+  migrationsTransactionMode: 'each'
+};
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Application } from "express";
 import cors from "cors";
+import "reflect-metadata";
 
 import { MainRouter } from "./routes/mainRouter";
 import errorHandler from "./controllers/error/errorController";
+import connectDatabase from "../src/config/database";
+
 
 const app: Application = express();
 
@@ -10,5 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", MainRouter);
 app.use(errorHandler);
+
+export async function init () {
+  await connectDatabase();
+}
 
 export default app;
