442a3ff4168be75ec291976fb944b7da78db8a54
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-30 03:40:38 -0300

    include migrations

--- src/migrations/1627602889475-Database.ts
+++ src/migrations/1627602889475-Database.ts
@@ -1,8 +1,8 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 
-export class Testando1627602889475 implements MigrationInterface {
-    name = 'Testando1627602889475'
+export class Database1627602889475 implements MigrationInterface {
+    name = 'Databse1627602889475'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "categories" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_24dbc6126a28ff948da33e97d3b" PRIMARY KEY ("id"))`);
