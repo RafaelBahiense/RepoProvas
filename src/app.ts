1cb905c221ee533457ed3d15d62ec96832222d82
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-08-01 19:49:59 -0300

    json error handler

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import "reflect-metadata";
 
 import { MainRouter } from "./routes/mainRouter";
-import errorHandler from "./controllers/error/errorController";
+import errorHandler, { jsonError} from "./controllers/error/errorController";
 import connectDatabase from "../src/config/database";
 
 
@@ -11,6 +11,7 @@ const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(jsonError);
 app.use("/api", MainRouter);
 app.use(errorHandler);
 
