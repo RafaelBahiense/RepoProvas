2e19bd23f431681d3d07badea1e09db534e67cf7
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-08-01 18:57:37 -0300

    tests joi schema

---
+++ src/schemas/schema.ts
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const testSchema = Joi.object({
+    name: Joi.string().min(1),
+    categoryId: Joi.number().min(1),
+    subjectId: Joi.number().min(1),
+    professorId: Joi.number().min(1),
+    link: Joi.string().uri(),
+});
