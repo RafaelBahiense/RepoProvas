5655f283798ab6767d9ea56102447840290a49e4
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-08-01 21:02:27 -0300

    subjects controller

---
+++ src/controllers/subjectsController.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+import * as subjectsService from "../services/subjectsService";
+
+export async function getAll(req: Request, res: Response, next: NextFunction) {
+  try {
+    const subjects = await subjectsService.getAll();
+    res.status(200).send(subjects);
+  } catch (e) {
+    next(e);
+  }
+}
