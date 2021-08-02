5d2452033673927f95cbc9c7b1f5293df5178225
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-08-01 21:02:35 -0300

    subjects service

---
+++ src/services/subjectsService.ts
@@ -0,0 +1,8 @@
+import { getRepository } from "typeorm";
+
+import Subject from "../entities/Subject";
+
+
+export async function getAll() {
+  return await getRepository(Subject).find({relations: ["professors"]});
+}
