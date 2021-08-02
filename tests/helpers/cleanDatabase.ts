b335c770d08a4957311c050217c45db3b93d83ee
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-08-02 10:57:19 -0300

    missing tables

--- tests/helpers/cleanDatabase.ts
+++ tests/helpers/cleanDatabase.ts
@@ -4,10 +4,13 @@ import Test from "../../src/entities/Test";
 import Category from "../../src/entities/Category";
 import Subject from "../../src/entities/Subject";
 import Professor from "../../src/entities/Professor";
+import Semester from "../../src/entities/Semester";
 
 export default async function cleanDB () {
   await getRepository(Test).query(`TRUNCATE TABLE tests RESTART IDENTITY CASCADE`);
   await getRepository(Category).query(`TRUNCATE TABLE categories RESTART IDENTITY CASCADE`);
   await getRepository(Subject).query(`TRUNCATE TABLE subjects RESTART IDENTITY CASCADE`);
+  await getRepository(Professor).query(`TRUNCATE TABLE professors RESTART IDENTITY CASCADE`);
   await getRepository(Professor).query(`TRUNCATE TABLE subjects_professors_professors RESTART IDENTITY CASCADE`);
+  await getRepository(Semester).query(`TRUNCATE TABLE semesters RESTART IDENTITY CASCADE`);
 }
